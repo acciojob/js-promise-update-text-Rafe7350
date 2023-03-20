3b5754e5e0c29f6ea393d0063c539fdb9cf7ccca
Rafe7350 <117887198+Rafe7350@users.noreply.github.com>
2023-03-20 10:43:09 +0530

    add

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+// assignment
+function func(){
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve("Hello, world!");
+		}, 1000);
+	});
+}
+func().then((value) => document.getElementById("output").innerText = value);
